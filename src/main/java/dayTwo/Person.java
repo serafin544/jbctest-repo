2d6cb3eb7f669968e4cab7b8cfa4d9ff4d6807d0
Serafin Escobar <serafinthedream@outlook.com>
2019-09-09 13:04:26 -0400

    first week

---
+++ src/main/java/dayTwo/Person.java
@@ -0,0 +1,96 @@
+package dayTwo;
+
+
+
+public class Person {
+    public static void main(String[] args) {
+
+        //
+
+        String s = scan.nextLine();
+        scan.nextLine();
+        //My own versions
+
+      /*  int carSpeed = 40;
+        // : is like false and ? is true
+        String result = (carSpeed > 45) ? "The car is speeding!! get em coppers"
+                :" Good fella, hes just crusing";
+        System.out.println(result);
+
+        int grade = 85;
+        if(grade > 69 ){
+            System.out.println("You passed!");
+
+        }else{
+            System.out.println("You failed !!");
+        }
+        int gradeOne = 60;
+        switch (gradeOne){
+            case  90:
+                System.out.println("A");
+                break;
+            case 80:
+                System.out.println("B");
+                break;
+            case 70:
+                System.out.println("C");
+                break;
+            default:
+                System.out.println("F");
+                break;
+
+        }
+*/
+/*        //Switch statemnets
+        int rating = 1;
+        switch (rating){
+            case 1:
+                System.out.println("biggie");
+                break;
+            case 2:
+                System.out.println("Jay z");
+                break;
+            case 3:
+                System.out.println("Nas");
+                break;
+            default:
+                System.out.println("fela kuri");
+        }*/
+
+       /* //Ternary Operator
+        int age = 19;
+        // : is like true
+        String result = (age < 18) ? "Cannot party with us, you little baby"
+                :" sure grab a brew";
+        System.out.println(result);
+*/
+
+
+/*
+       //Review from yesterday
+        System.out.println("We all know this by now");
+        int x = 230;
+        double y = (double) x;
+        System.out.println(y);
+
+        String firstName = "kool";
+        String lastName = "G Rap";
+        System.out.println(firstName+ " " + lastName);
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+        System.out.println(fullName.length());
+
+
+
+
+        //StringBuilder
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("wacka").append(" flocka");
+        System.out.println(stringBuilder);
+*/
+
+
+
+
+    }
+}
---
+++ src/main/java/weekendhomework/main.java
@@ -0,0 +1,4 @@
+package weekendhomework;
+
+public class main {
+}
